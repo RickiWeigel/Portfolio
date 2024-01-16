717746a4cd61c494e417364e8356aa498bac1787
Ricki W. <ricki.weigel@gmx.de>
2024-01-16 18:34:47 +0100

    finished landing-page designe-component

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./../../src/app/shared/components/header/header.component";
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,HeaderComponent,LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
