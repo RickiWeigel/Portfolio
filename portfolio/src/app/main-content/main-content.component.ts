cfb4472d38dca8c9d22b123f606af22bf1accc4f
Ricki W. <ricki.weigel@gmx.de>
2024-01-23 15:28:09 +0100

    added skills-component

--- portfolio/src/app/main-content/main-content.component.ts
+++ portfolio/src/app/main-content/main-content.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeaderComponent, LandingPageComponent, AboutMeComponent],
+  imports: [HeaderComponent, LandingPageComponent, AboutMeComponent, SkillsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
