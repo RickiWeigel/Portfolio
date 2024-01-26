60339e356bd46f75b6cb93a744358c6a9baa709a
Ricki W. <ricki.weigel@gmx.de>
2024-01-26 16:58:33 +0100

    added and completed my-projects-component

--- portfolio/src/app/main-content/main-content.component.ts
+++ portfolio/src/app/main-content/main-content.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from '../shared/components/header/header.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
+import { MyProjectsComponent } from './my-projects/my-projects.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeaderComponent, LandingPageComponent, AboutMeComponent, SkillsComponent],
+  imports: [HeaderComponent, LandingPageComponent, AboutMeComponent, SkillsComponent,MyProjectsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
