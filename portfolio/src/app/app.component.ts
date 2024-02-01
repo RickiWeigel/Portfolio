e8286bb7b6769e0b609c03a4919af2d0009967cb
Ricki W. <ricki.weigel@gmx.de>
2024-02-01 15:22:24 +0100

    added footer

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,MainContentComponent,HeaderComponent,],
+  imports: [CommonModule, RouterOutlet,MainContentComponent,HeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
