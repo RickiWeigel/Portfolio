e9a3d2f929c728ea18b48e5c930ee905c494b12d
Ricki W. <ricki.weigel@gmx.de>
2024-11-18 10:11:17 +0100

    (fix)

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MainContentComponent } from './main-content/main-content.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 
@@ -9,7 +8,7 @@ import { FooterComponent } from './shared/components/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,MainContentComponent,HeaderComponent,FooterComponent],
+  imports: [CommonModule, RouterOutlet,HeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
